fa2ace6463662d03110cc93a60e45fa89077f862
BrunoC2711 <brunoalbuquerque1998@gmail.com>
2025-02-15 19:08:22 -0300

    Application to Redis in my project

--- src/app/jobs/RegistrationMail.js
+++ src/app/jobs/RegistrationMail.js
@@ -3,13 +3,14 @@ import Mail from '../lib/Mail';
 export default {
     key: 'RegistrationMail',
     options: {
-
+        delay: 5000,
+        priority: 3,
     },
     async handle({ data }) {
         const {user} = data;
         
         await Mail.sendMail({
-            from: 'Me <bruno@gmail.com',
+            from: 'TEST_MAIL <mailtesting@test.com.br>',
             to: `${user.name} <${user.email}>`,
             subject: 'User register',
             html: `Hello, ${user.name}, welcome!!`
